a7480e8225facdf8e2ed03327785dd2c50436c85
gusev-stankin-idm1804 <44869844+gusev-stankin-idm1804@users.noreply.github.com>
2022-03-27 23:05:28 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function() {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })    
+}
+menuBody.addEventListener('click', function(event) {
+    if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active'); 
+    }        
+})
